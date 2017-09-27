77360b5f83b3500a874efe35ead7b9c9a839dc67
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-09-27 16:48:25 +0300

    Fixed binding

--- darwin/foundation/nsdata.nim
+++ darwin/foundation/nsdata.nim
@@ -4,7 +4,7 @@ type NSData* = ptr object of NSObject
 
 proc length*(p: NSData): int {.objc: "length".}
 proc getBytes*(self: NSData, buffer: pointer, length: int) {.objc: "getBytes:length:".}
-proc withBytes(n: typedesc[NSData], bytes: pointer, length: int) {.objc: "dataWithBytes:length:".}
+proc withBytes(n: typedesc[NSData], bytes: pointer, length: int): NSData {.objc: "dataWithBytes:length:".}
 
 proc dataWithBytes*(bytes: cstring, length: int): NSData {.inline.} =
     NSData.withBytes(bytes, length)
