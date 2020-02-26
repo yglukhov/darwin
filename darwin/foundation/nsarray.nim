b929c00218b2b9aa195b81936fcb5b1cf4196ac8
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-02-27 00:58:49 +0200

    NSArray fix

--- darwin/foundation/nsarray.nim
+++ darwin/foundation/nsarray.nim
@@ -12,6 +12,7 @@ proc objcClass(t: typedesc[NSMutableArrayAbstract]): auto {.inline.} = objcClass
 proc objectAtIndex*(a: NSArrayAbstract, i: int): NSObject {.objc: "objectAtIndex:".}
 proc withObjectsAndCount(n: typedesc[NSArrayAbstract], objs: pointer, count: int): NSArrayAbstract {.objc: "arrayWithObjects:count:".}
 proc withObjectsAndCount(n: typedesc[NSMutableArrayAbstract], objs: pointer, count: int): NSArrayAbstract {.objc: "arrayWithObjects:count:".}
+proc withCapacity(n: typedesc[NSMutableArrayAbstract], c: int): NSArrayAbstract {.objc: "arrayWithCapacity:".}
 proc arrayWithObjects*[T](objs: varargs[T]): NSArray[T] =
     cast[NSArray[T]](NSArrayAbstract.withObjectsAndCount(unsafeAddr objs[0], objs.len))
 
@@ -20,8 +21,11 @@ proc newMutableArrayAbstract(n: typedesc[NSMutableArrayAbstract]): NSMutableArra
 proc newMutableArray*[T](): NSMutableArray[T] {.inline.} =
     cast[NSMutableArray[T]](newMutableArrayAbstract(NSMutableArrayAbstract))
 
-proc mutableArrayWithObjects*[T](objs: varargs[T]): NSArray[T] =
-    cast[NSArray[T]](NSMutableArrayAbstract.withObjectsAndCount(unsafeAddr objs[0], objs.len))
+proc mutableArrayWithCapacity*[T](capacity: int): NSMutableArray[T] {.inline.} =
+    cast[NSMutableArray[T]](withCapacity(NSMutableArrayAbstract, capacity))
+
+proc mutableArrayWithObjects*[T](objs: varargs[T]): NSMutableArray[T] {.inline.} =
+    cast[NSMutableArray[T]](NSMutableArrayAbstract.withObjectsAndCount(unsafeAddr objs[0], objs.len))
 
 proc addObject*(a: NSMutableArrayAbstract, o: NSObject) {.objc: "addObject:".}
 
@@ -31,11 +35,11 @@ proc add*[T](a: NSMutableArray[T], o: T) {.inline.} =
 proc count*(a: NSArrayAbstract): int {.objc: "count".}
 proc len*(a: NSArrayAbstract): int {.inline.} = a.count
 
-proc setObject(a: NSMutableArrayAbstract, idx: int, v: NSObject) {.objc: "setObject:atIndexedSubscript:".}
+proc setObject(a: NSMutableArrayAbstract, v: NSObject, idx: int) {.objc: "setObject:atIndexedSubscript:".}
 
 proc `[]`*[T](a: NSArray[T], idx: int): T {.inline.} = cast[T](objectAtIndex(a, idx))
 proc `[]=`*[T](a: NSMutableArray[T], idx: int, v: T) {.inline.} =
-    setObject(cast[NSMutableArrayAbstract](a), idx, v)
+    setObject(cast[NSMutableArrayAbstract](a), v, idx)
 
 iterator items*[T](a: NSArray[T]): T =
     let ln = a.len
