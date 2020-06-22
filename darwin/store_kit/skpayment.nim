f4b8175df648564e17539d6370180977d12b311f
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-06-22 15:40:19 +0300

    Fixed setApplicationUsername signature

--- darwin/store_kit/skpayment.nim
+++ darwin/store_kit/skpayment.nim
@@ -6,4 +6,4 @@ import skproduct
 type SKPayment* = ptr object of NSObject
 
 proc withProduct*(t: typedesc[SKPayment], p: SKProduct): SKPayment {.objc: "paymentWithProduct:".}
-proc setApplicationUsername*(p: SKProduct, n: NSString) {.objc: "setApplicationUsername:".}
+proc setApplicationUsername*(p: SKPayment, n: NSString) {.objc: "setApplicationUsername:".}
