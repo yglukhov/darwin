066f0b0c313d0e04c38c44912f7bdc86d851434b
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-06-22 21:16:11 +0300

    Fixed addTransactionObserver signature

--- darwin/store_kit/skpayment_queue.nim
+++ darwin/store_kit/skpayment_queue.nim
@@ -6,4 +6,4 @@ type SKPaymentQueue* = ptr object of NSObject
 
 proc defaultQueue*(t: typedesc[SKPaymentQueue]): SKPaymentQueue {.objc.}
 proc add*(q: SKPaymentQueue, p: SKPayment) {.objc: "addPayment:".}
-proc addObserver*(q: SKPaymentQueue, a: NSObject) {.objc: "addObserver:".}
+proc addTransactionObserver*(q: SKPaymentQueue, a: NSObject) {.objc: "addTransactionObserver:".}
