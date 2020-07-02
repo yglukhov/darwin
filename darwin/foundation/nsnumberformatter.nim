1461fd56ac035d7a50226adff46f5d2ce2b2767d
nikitincrime <kcinris@gmail.com>
2020-07-02 17:12:13 +0300

    add nsNumberFormatter (#6)
    Co-authored-by: Sergey Nikitin <snikitin@oftengames.com>

---
+++ darwin/foundation/nsnumberformatter.nim
@@ -0,0 +1,17 @@
+import ../objc/runtime
+import ../foundation/[nsnumber, nslocale]
+
+const kCFNumberFormatterCurrencyStyle = 2
+const NSNumberFormatterBehavior10_4* = 1040
+
+type NSNumberFormatter* = ptr object of NSObject
+
+type NSNumberFormatterStyle* = enum
+    NSNumberFormatterCurrencyStyle = kCFNumberFormatterCurrencyStyle
+
+proc stringFromNumber*(f: NSNumberFormatter, p: NSNumber): NSString {.objc: "stringFromNumber:".}
+
+proc setFormatterBehavior*(formatter: NSNumberFormatter): NSString {.objc.}
+proc setNumberStyle*(formatter: NSNumberFormatter,
+    style: NSNumberFormatterStyle) {.objc:"setNumberStyle:".}
+proc setLocale*(formatter: NSNumberFormatter, locale: NSLocale) {.objc:"setLocale".}
