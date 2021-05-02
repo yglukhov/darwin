36207735617ef5c3da1a618b56f42ef08264d52a
Bung <crc32@qq.com>
2021-05-02 12:10:28 +0800

    typo (#11)

--- darwin/app_kit.nim
+++ darwin/app_kit.nim
@@ -1,5 +1,5 @@
 import foundation
-import app_kit / [ nsscreen, nsview, nsevent, nscursor, nspasteboard, nswindow, nsapplication, nsmenu, nsresponder, nstext, nstextview, nswindowcontroller, nsdragoperation ]
-export foundation, nsscreen, nsview, nsevent, nscursor, nspasteboard, nswindow, nsapplication, nsmenu, nsresponder, nstext, nstextview, nswindowcontroller, nsdragoperation
+import app_kit / [ nsscreen, nsview, nsevent, nscursor, nspasteboard, nswindow, nsapplication, nsmenu, nsresponder, nstext, nstextview, nswindowcontroller, nsdragoperation, nsdragginginfo ]
+export foundation, nsscreen, nsview, nsevent, nscursor, nspasteboard, nswindow, nsapplication, nsmenu, nsresponder, nstext, nstextview, nswindowcontroller, nsdragoperation, nsdragginginfo
 
 {.passL:"-framework AppKit".}
