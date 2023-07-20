1648276ecc6c0051867a216db8f4bbd960c2b46e
bung87 <crc32@qq.com>
2023-07-20 20:09:21 +0800

    add NSApp

--- darwin/app_kit/nsapplication.nim
+++ darwin/app_kit/nsapplication.nim
@@ -1,7 +1,10 @@
 import ./nsresponder
+
 type 
   NSApplication* = ptr object of NSResponder
   NSApplicationActivationPolicy* {.size: sizeof(uint).} = enum
     NSApplicationActivationPolicyRegular
     NSApplicationActivationPolicyAccessory
     NSApplicationActivationPolicyProhibited
+
+var NSApp* {.importc.}: pointer
