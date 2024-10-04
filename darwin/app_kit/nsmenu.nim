30906da03a3b8dc21ab2d6217897928cc9fab239
Bung <crc32@qq.com>
2024-10-04 13:51:10 +0800

    `NSMenuItem` methods (#31)

--- darwin/app_kit/nsmenu.nim
+++ darwin/app_kit/nsmenu.nim
@@ -16,3 +16,21 @@ proc addItem*(self: NSMenu, item: NSMenuItem)  {.objc: "addItem:".}
 proc setKeyEquivalentModifierMask*(self: NSMenuItem, mask: NSEventModifierFlags) {.objc: "setKeyEquivalentModifierMask:".}
 
 proc separatorItem*(self: typedesc[NSMenuItem]): NSMenuItem {.objc.}
+
+proc setTarget*(self: NSMenuItem, target: Id) {.objc: "setTarget:".}
+
+proc setAction*(self: NSMenuItem, action: SEL) {.objc: "setAction:".}
+
+proc setEnabled*(self: NSMenuItem, enabled: BOOL) {.objc: "setEnabled:".}
+
+proc title*(self: NSMenuItem): NSString {.objc: "title".}
+
+proc setTitle*(self: NSMenuItem, title: NSString) {.objc: "setTitle:".}
+
+proc setSeparatorItem*(self: NSMenuItem, b: BOOL): NSMenuItem {.objc: "setSeparatorItem:".}
+
+proc isSeparatorItem*(self: NSMenuItem): BOOL {.objc: "isSeparatorItem".}
+
+proc keyEquivalent*(self: NSMenuItem): NSString {.objc: "keyEquivalent".}
+
+proc hasSubmenu*(self: NSMenuItem): BOOL {.objc: "hasSubmenu".}
