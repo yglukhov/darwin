644d50754278443acf085b7a1b36474d31ffb590
Jaremy Creechley <creechley@gmail.com>
2026-02-10 19:46:20 -0700

    import camlayer types

--- darwin/quartz_core/cametal_layer.nim
+++ darwin/quartz_core/cametal_layer.nim
@@ -1,4 +1,22 @@
+import ../objc/runtime
 import ./calayer
+import ../core_graphics/cggeometry
+import metalx/metal
+
+{.passL: "-framework QuartzCore".}
 
 type
   CAMetalLayer* = ptr object of CALayer
+  CAMetalDrawable* = ptr object of NSObject
+
+proc device*(l: CAMetalLayer): MTLDevice {.objc: "device".}
+proc setDevice*(l: CAMetalLayer, d: MTLDevice) {.objc: "setDevice:".}
+proc pixelFormat*(l: CAMetalLayer): MTLPixelFormat {.objc: "pixelFormat".}
+proc setPixelFormat*(l: CAMetalLayer, f: MTLPixelFormat) {.objc: "setPixelFormat:".}
+proc drawableSize*(l: CAMetalLayer): CGSize {.objc: "drawableSize".}
+proc setDrawableSize*(l: CAMetalLayer, s: CGSize) {.objc: "setDrawableSize:".}
+proc frame*(l: CAMetalLayer): CGRect {.objc: "frame".}
+proc setFrame*(l: CAMetalLayer, r: CGRect) {.objc: "setFrame:".}
+proc nextDrawable*(l: CAMetalLayer): CAMetalDrawable {.objc: "nextDrawable".}
+
+proc texture*(d: CAMetalDrawable): MTLTexture {.objc: "texture".}
