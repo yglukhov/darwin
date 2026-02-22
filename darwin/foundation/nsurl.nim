7611978e54d9c46b48098a24769d6a1b47485c21
Bung <crc32@qq.com>
2026-02-23 00:54:53 +0800

    Speech (#48)
    * Add fileURLWithPath to NSURL
    
    * Add Speech framework bindings
    
    * fix

--- darwin/foundation/nsurl.nim
+++ darwin/foundation/nsurl.nim
@@ -3,4 +3,5 @@ import ../objc/runtime
 type NSURL* = ptr object of NSObject
 
 proc URLWithString*(self: typedesc[NSURL], str: NSString): NSURL {.objc: "URLWithString:".}
+proc fileURLWithPath*(self: typedesc[NSURL], path: NSString): NSURL {.objc: "fileURLWithPath:".}
 proc path*(self: NSURL): NSString {.objc.}
---
+++ darwin/speech.nim
@@ -0,0 +1,5 @@
+import speech/[sfspeechrecognizer, sfspeechrecognitionrequest, sfspeechrecognitionresult, sfspeechrecognitiontask, sfspeechrecognitionmetadata]
+import foundation/[nsstring, nsarray, nsurl, nsdata]
+
+export sfspeechrecognizer, sfspeechrecognitionrequest, sfspeechrecognitionresult, sfspeechrecognitiontask, sfspeechrecognitionmetadata
+export NSString, NSArray, NSURL, NSData
