dc27b4c7244135a2938ce6b94aeca78bf52148a4
bung87 <crc32@qq.com>
2026-02-23 13:01:48 +0800

    Add CFURL and CTFont bindings for font URL access
    - Add CFURLRef type alias and getFileSystemRepresentation proc to cfurl.nim
    - Add kCTFontURLAttribute and copyAttribute to ctfont.nim
    
    These bindings enable retrieving the file system URL of a font.

--- darwin/core_foundation/cfurl.nim
+++ darwin/core_foundation/cfurl.nim
@@ -2,6 +2,7 @@ import cfbase, cfstring
 
 type
     CFURL* = ptr object of CFObject
+    CFURLRef* = CFURL
     CFURLPathStyle* = enum
         kCFURLPOSIXPathStyle = 0
         kCFURLWindowsPathStyle = 2
@@ -12,3 +13,4 @@ proc CFURLGetTypeID*(): CFTypeID {.importc.}
 proc CFURLCreateWithBytes*(allocator: CFAllocator, URLBytes: pointer, length: CFIndex, encoding: CFStringEncoding, baseURL: CFURL): CFURL {.importc.}
 proc CFURLCreateWithString*(allocator: CFAllocator, URLString: CFString, baseURL: CFURL): CFURL {.importc.}
 proc CFURLCreateWithFileSystemPath*(allocator: CFAllocator, filePath: CFString, pathStyle: CFURLPathStyle, isDirectory: Boolean): CFURL {.importc.}
+proc getFileSystemRepresentation*(url: CFURLRef, resolveAgainstBase: Boolean, buffer: ptr uint8, maxBufLen: CFIndex): Boolean {.importc: "CFURLGetFileSystemRepresentation".}
