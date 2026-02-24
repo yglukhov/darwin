198362373f94eb2ac123a901f74963d15ec0d2b7
Bung <crc32@qq.com>
2026-02-24 16:09:06 +0800

    Add NSFileManager binding to foundation (#55)

--- darwin/foundation.nim
+++ darwin/foundation.nim
@@ -1,11 +1,13 @@
 import foundation / [nsstring, nsattributedstring, nsgeometry, nsrange, nsarray, nsdata, nsset, nsenumerator,
     nserror, nsdate, nsdictionary, nsnumber, nsdecimal_number, nslocale,
     nspath_utilities, nsprocessinfo, nsurl, nsurlrequest, nscoder, nsbundle,
-    nsrunloop, nsstream, nsnotification, nsurlresponse, nsautoreleasepool, nsvalue]
+    nsrunloop, nsstream, nsnotification, nsurlresponse, nsautoreleasepool, nsvalue,
+    nsfilemanager]
 export nsstring, nsattributedstring, nsgeometry, nsrange, nsarray, nsdata, nsset, nsenumerator, nserror,
     nsdate, nsdictionary, nsnumber, nsdecimal_number, nslocale,
     nspath_utilities, nsprocessinfo, nsurl, nsurlrequest, nscoder, nsbundle,
-    nsrunloop, nsstream, nsnotification, nsurlresponse, nsautoreleasepool, nsvalue
+    nsrunloop, nsstream, nsnotification, nsurlresponse, nsautoreleasepool, nsvalue,
+    nsfilemanager
 
 import objc/runtime
 export NSObject, NSLog, isKindOfClass, retain, release, alloc, init
