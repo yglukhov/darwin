0d9822d8dcba1e128869dc8b75168d456b2baa24
bung87 <crc32@qq.com>
2026-03-01 13:02:38 +0800

    change back constructor names

--- darwin/core_foundation/cfurl.nim
+++ darwin/core_foundation/cfurl.nim
@@ -9,7 +9,7 @@ type
 
 proc CFURLGetTypeID*(): CFTypeID {.importc.}
 
-proc createWithBytes*(allocator: CFAllocator, URLBytes: pointer, length: CFIndex, encoding: CFStringEncoding, baseURL: CFURL): CFURL {.importc: "CFURLCreateWithBytes".}
-proc createWithString*(allocator: CFAllocator, URLString: CFString, baseURL: CFURL): CFURL {.importc: "CFURLCreateWithString".}
-proc createWithFileSystemPath*(allocator: CFAllocator, filePath: CFString, pathStyle: CFURLPathStyle, isDirectory: Boolean): CFURL {.importc: "CFURLCreateWithFileSystemPath".}
+proc CFURLCreateWithBytes*(allocator: CFAllocator, URLBytes: pointer, length: CFIndex, encoding: CFStringEncoding, baseURL: CFURL): CFURL {.importc: "CFURLCreateWithBytes".}
+proc CFURLCreateWithString*(allocator: CFAllocator, URLString: CFString, baseURL: CFURL): CFURL {.importc: "CFURLCreateWithString".}
+proc CFURLCreateWithFileSystemPath*(allocator: CFAllocator, filePath: CFString, pathStyle: CFURLPathStyle, isDirectory: Boolean): CFURL {.importc: "CFURLCreateWithFileSystemPath".}
 proc getFileSystemRepresentation*(url: CFURL, resolveAgainstBase: Boolean, buffer: ptr uint8, maxBufLen: CFIndex): Boolean {.importc: "CFURLGetFileSystemRepresentation".}
