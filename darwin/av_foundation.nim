d9aad388ef93d2170c6185ab874ae6345f49196f
bung87 <crc32@qq.com>
2026-03-23 23:51:52 +0800

    Export avaudiomixernode from av_foundation package

--- darwin/av_foundation.nim
+++ darwin/av_foundation.nim
@@ -1,4 +1,4 @@
-import av_foundation / [avaudioplayer, avaudioplayerdelegate, avaudioformat, avaudioengine, avaudioplayernode, avaudiopcmbuffer, avaudionode]
-export avaudioplayer, avaudioplayerdelegate, avaudioformat, avaudioengine, avaudioplayernode, avaudiopcmbuffer, avaudionode
+import av_foundation / [avaudioplayer, avaudioplayerdelegate, avaudioformat, avaudioengine, avaudioplayernode, avaudiopcmbuffer, avaudionode, avaudiomixernode]
+export avaudioplayer, avaudioplayerdelegate, avaudioformat, avaudioengine, avaudioplayernode, avaudiopcmbuffer, avaudionode, avaudiomixernode
 
 {.passL: "-framework AVFoundation".}
