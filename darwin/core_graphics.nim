2a98e87ee8a82cc0e866cd33c3a4b301eeece6d0
bung87 <crc32@qq.com>
2026-03-25 05:48:31 +0800

    add `CGDataConsumer` and missing of cgcontext

--- darwin/core_graphics.nim
+++ darwin/core_graphics.nim
@@ -1,2 +1,4 @@
-import core_graphics / [cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage]
-export cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage
+{.passL: "-framework CoreGraphics".}
+
+import core_graphics / [cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage, cgdataconsumer]
+export cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage, cgdataconsumer
