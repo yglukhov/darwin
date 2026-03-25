2732f963fddcbd2bfefe380620bc88fc0aaa1bdf
bung87 <crc32@qq.com>
2026-03-26 02:40:55 +0800

    export types

--- darwin/core_graphics.nim
+++ darwin/core_graphics.nim
@@ -1,4 +1,7 @@
 {.passL: "-framework CoreGraphics".}
 
 import core_graphics / [cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage, cgdataconsumer]
+import core_graphics / [cgpdfobject, cgpdfarray, cgpdfdictionary, cgpdfstream, cgpdfstring, cgpdfdocument, cgpdfpage, cgpdfcontext]
+
 export cggeometry, cgfont, cgcolor_space, cgcontext, cgbitmap_context, cgaffine_transform, cgpath, cgimage, cgdataconsumer
+export cgpdfobject, cgpdfarray, cgpdfdictionary, cgpdfstream, cgpdfstring, cgpdfdocument, cgpdfpage, cgpdfcontext
