db5f953e09c20d8e070527e7a411b7635012c833
bung87 <crc32@qq.com>
2026-03-26 02:16:34 +0800

    Add NSColor factory methods and common colors
    - Add colorWithDeviceRed:green:blue:alpha:
    - Add colorWithRed:green:blue:alpha:
    - Add blackColor, whiteColor, clearColor factory methods
    - Import cggeometry for CGFloat type

--- darwin/app_kit/nscolor.nim
+++ darwin/app_kit/nscolor.nim
@@ -1,4 +1,14 @@
 import ../objc/runtime
+import ../core_graphics/cggeometry
 
-type 
+type
   NSColor* = ptr object of NSObject
+
+# Factory methods
+proc colorWithDeviceRed*(self: typedesc[NSColor], red, green, blue, alpha: CGFloat): NSColor {.objc: "colorWithDeviceRed:green:blue:alpha:".}
+proc colorWithRed*(self: typedesc[NSColor], red, green, blue, alpha: CGFloat): NSColor {.objc: "colorWithRed:green:blue:alpha:".}
+
+# Common colors
+proc blackColor*(self: typedesc[NSColor]): NSColor {.objc: "blackColor".}
+proc whiteColor*(self: typedesc[NSColor]): NSColor {.objc: "whiteColor".}
+proc clearColor*(self: typedesc[NSColor]): NSColor {.objc: "clearColor".}
