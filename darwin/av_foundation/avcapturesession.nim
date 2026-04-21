3560795c3cf74470936151da0d452e376a315233
Bung <crc32@qq.com>
2026-04-21 23:30:31 +0800

    feat: add AVFoundation video capture bindings (#86)
    Add AVCaptureSession and related classes for camera/video input capture.
    
    - av_foundation.nim: export new capture modules
    - avcapturesession.nim: session lifecycle, input/output management
    - avcapturedevice.nim: device discovery, position, media types
    - avcaptureinput.nim: AVCaptureDeviceInput
    - avcaptureoutput.nim: AVCaptureOutput base type
    - avcapturevideodataoutput.nim: video data output with delegate support
    - avcaptureconnection.nim: connection orientation control

---
+++ darwin/av_foundation/avcapturesession.nim
@@ -0,0 +1,20 @@
+import ../objc/runtime
+import ../foundation/nsarray
+import avcaptureinput
+import avcapturevideodataoutput
+
+type
+  AVCaptureSession* = ptr object of NSObject
+
+# AVCaptureSession methods
+proc alloc*(t: typedesc[AVCaptureSession]): AVCaptureSession {.objc: "alloc".}
+proc init*(s: AVCaptureSession): AVCaptureSession {.objc: "init".}
+proc startRunning*(s: AVCaptureSession) {.objc.}
+proc stopRunning*(s: AVCaptureSession) {.objc.}
+proc isRunning*(s: AVCaptureSession): BOOL {.objc.}
+proc canAddInput*(s: AVCaptureSession, input: AVCaptureDeviceInput): BOOL {.objc: "canAddInput:".}
+proc addInput*(s: AVCaptureSession, input: AVCaptureDeviceInput) {.objc: "addInput:".}
+proc canAddOutput*(s: AVCaptureSession, output: AVCaptureVideoDataOutput): BOOL {.objc: "canAddOutput:".}
+proc addOutput*(s: AVCaptureSession, output: AVCaptureVideoDataOutput) {.objc: "addOutput:".}
+proc beginConfiguration*(s: AVCaptureSession) {.objc.}
+proc commitConfiguration*(s: AVCaptureSession) {.objc.}
